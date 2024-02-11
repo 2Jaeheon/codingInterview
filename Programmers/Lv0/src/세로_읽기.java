21d91fbcc9012dfd4b697ff157d3d78a6e70aa65
springPotatoes <121741455+springPotatoes@users.noreply.github.com>
2024-02-12 00:32:30 +0900

    Solve: lv0 세로 읽기

---
+++ Programmers/Lv0/src/세로_읽기.java
@@ -0,0 +1,12 @@
+public class 세로_읽기 {
+
+    public String solution(String my_string, int m, int c) {
+        String answer = "";
+
+        for (int i = c - 1; i < my_string.length(); i += m) {
+            answer += my_string.charAt(i) + "";
+        }
+
+        return answer;
+    }
+}
