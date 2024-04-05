5f9a3e187d1a991f7f70a7a8bd921e073e26aa8a
JaeheonLee <121741455+2Jaeheon@users.noreply.github.com>
2024-04-05 16:59:44 +0900

    Solve: lv0 컨트롤 제트

---
+++ Programmers/Lv0/src/컨트롤_제트.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class 컨트롤_제트 {
+
+    class Solution {
+
+        public int solution(String s) {
+            int answer = 0;
+            String[] arr = s.split(" ");
+
+            for (int i = 0; i < arr.length; i++) {
+                if (arr[i].equals("Z")) {
+                    answer -= Integer.parseInt(arr[i - 1]);
+                } else {
+                    answer += Integer.parseInt(arr[i]);
+                }
+            }
+            return answer;
+        }
+    }
+}
