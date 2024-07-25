d2dfd4d2d756ed83be79e71540fbd7e8ddacec2c
JaeheonLee <121741455+2Jaeheon@users.noreply.github.com>
2024-07-25 16:14:34 +0900

    ADD: 코딩테스트 문제풀이 연습

--- Practice/src/Array/모의고사.java
+++ Practice/src/Array/모의고사.java
@@ -69,10 +69,14 @@ public class 모의고사 {
     }
 
     public static int[] solution2(int[] answers) {
-        int[][] pattern = {{1, 2, 3, 4, 5}, {2, 1, 2, 3, 2, 4, 2, 5},
-            {3, 3, 1, 1, 2, 2, 4, 4, 5, 5}};
+        int[][] pattern = {
+            {1, 2, 3, 4, 5},
+            {2, 1, 2, 3, 2, 4, 2, 5},
+            {3, 3, 1, 1, 2, 2, 4, 4, 5, 5}
+        };
 
         int[] scores = new int[3];
+
         for (int i = 0; i < answers.length; i++) {
             for (int j = 0; j < pattern.length; j++) {
                 if (answers[i] == pattern[j][i % pattern[j].length]) {
