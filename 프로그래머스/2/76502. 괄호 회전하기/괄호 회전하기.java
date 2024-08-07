ef8e25776f764e4eede8f3d3c8cd4f767828aa4b
JaeheonLee <121741455+2Jaeheon@users.noreply.github.com>
2024-08-07 17:53:12 +0900

    [level 2] Title: 괄호 회전하기, Time: 35.41 ms, Memory: 78.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
@@ -0,0 +1,36 @@
+import java.util.*;
+class Solution {
+    public static int solution(String s) {
+        ArrayDeque<Character> queue = new ArrayDeque<>();
+        int count = 0;
+        for (int i = 0; i < s.length(); i++) {
+            queue.add(s.charAt(i));
+        }
+
+        for (int i = 0; i < s.length(); i++) {
+            if(isCorrect(queue)){
+                count++;
+            }
+            queue.addLast(queue.pollFirst());
+        }
+        return count;
+    }
+
+    public static boolean isCorrect(ArrayDeque<Character> queue) {
+        ArrayDeque<Character> stack = new ArrayDeque<>();
+        for (Character ch : queue) {
+            if (ch == '(' || ch == '[' || ch == '{') {
+                stack.push(ch);
+            } else if (ch == ')' && !stack.isEmpty() && stack.peek() == '(') {
+                stack.pop();
+            } else if (ch == ']' && !stack.isEmpty() && stack.peek() == '[') {
+                stack.pop();
+            } else if (ch == '}' && !stack.isEmpty() && stack.peek() == '{') {
+                stack.pop();
+            } else {
+                return false; 
+            }
+        }
+        return stack.isEmpty(); 
+    }
+}
