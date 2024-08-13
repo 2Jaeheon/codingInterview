21ee595d9f4c6d2484fed13fe39eb0a1a720ae44
JaeheonLee <121741455+2Jaeheon@users.noreply.github.com>
2024-08-13 15:23:34 +0900

    FIX: pattern change

--- Practice/src/Array/모의고사.java
+++ Practice/src/Array/모의고사.java
@@ -15,10 +15,10 @@ public class 모의고사 {
     }
 
     public static int[] solution(int[] answers) {
-        //First Student
         int firstStudent = 1;
         int[] hits = new int[3];
 
+        //First Student
         for (int i = 0; i < answers.length; i++) {
             if (firstStudent == answers[i]) {
                 hits[0]++;
@@ -28,6 +28,8 @@ public class 모의고사 {
                 firstStudent = 1;
             }
         }
+
+        //Second Student
         int secondStudent = 1;
         for (int i = 0; i < answers.length; i++) {
             if (i % 2 == 0 && 2 == answers[i]) {
@@ -42,20 +44,40 @@ public class 모의고사 {
                 } else if (secondStudent == 6) {
                     secondStudent = 1;
                 }
-
             }
         }
 
         //third Student
-        int[] thirdPattern = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
+        int thirdStudent = 3;
+        int count = 0;
         for (int i = 0; i < answers.length; i++) {
-            if (answers[i] == thirdPattern[i % thirdPattern.length]) {
+            if (answers[i] == thirdStudent) {
                 hits[2]++;
+            }
+            count++;
+            if (count == 2) {
+                count = 0;
+                switch (thirdStudent) {
+                    case 3:
+                        thirdStudent = 1;
+                        break;
+                    case 1:
+                        thirdStudent = 2;
+                        break;
+                    case 2:
+                        thirdStudent = 4;
+                        break;
+                    case 4:
+                        thirdStudent = 5;
+                        break;
+                    case 5:
+                        thirdStudent = 3;
+                        break;
+                }
             }
         }
 
         // 가장 많이 맞힌 학생 찾기
-        //int maxHits = Math.max(hits[0], Math.max(hits[1], hits[2]));
         int maxHits = Arrays.stream(hits).max().getAsInt();
         ArrayList<Integer> topStudents = new ArrayList<>();
         for (int i = 0; i < hits.length; i++) {
