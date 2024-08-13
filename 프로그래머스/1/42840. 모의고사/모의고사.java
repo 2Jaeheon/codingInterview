2caad22a86e9e92686b48d1028b05e3543ab2778
JaeheonLee <121741455+2Jaeheon@users.noreply.github.com>
2024-08-13 15:23:00 +0900

    [level 1] Title: 모의고사, Time: 21.12 ms, Memory: 76.2 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.java
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -1,10 +1,10 @@
 import java.util.*;
 class Solution {
     public static int[] solution(int[] answers) {
-        //First Student
         int firstStudent = 1;
         int[] hits = new int[3];
 
+        //First Student
         for (int i = 0; i < answers.length; i++) {
             if (firstStudent == answers[i]) {
                 hits[0]++;
@@ -14,6 +14,7 @@ class Solution {
                 firstStudent = 1;
             }
         }
+
         int secondStudent = 1;
         for (int i = 0; i < answers.length; i++) {
             if (i % 2 == 0 && 2 == answers[i]) {
@@ -28,23 +29,31 @@ class Solution {
                 } else if (secondStudent == 6) {
                     secondStudent = 1;
                 }
-
             }
         }
+
+        //third Student
         int thirdStudent = 3;
         int count = 0;
-        boolean isFirst = true;
-
-        // 세 번째 학생 패턴
-        int[] thirdPattern = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
         for (int i = 0; i < answers.length; i++) {
-            if (answers[i] == thirdPattern[i % thirdPattern.length]) {
+            if (answers[i] == thirdStudent) {
                 hits[2]++;
+            }
+            count++;
+            if (count == 2) {
+                count = 0;
+                switch(thirdStudent) {
+                    case 3: thirdStudent = 1; break;
+                    case 1: thirdStudent = 2; break;
+                    case 2: thirdStudent = 4; break;
+                    case 4: thirdStudent = 5; break;
+                    case 5: thirdStudent = 3; break;
+                }
             }
         }
 
         // 가장 많이 맞힌 학생 찾기
-        int maxHits = Math.max(hits[0], Math.max(hits[1], hits[2]));
+        int maxHits = Arrays.stream(hits).max().getAsInt();
         ArrayList<Integer> topStudents = new ArrayList<>();
         for (int i = 0; i < hits.length; i++) {
             if (hits[i] == maxHits) {
@@ -53,6 +62,7 @@ class Solution {
         }
 
         // 리스트를 배열로 변환하여 반환
-        return topStudents.stream().mapToInt(i -> i).toArray();
+        return topStudents.stream().mapToInt(Integer::intValue).toArray();
     }
+
 }
