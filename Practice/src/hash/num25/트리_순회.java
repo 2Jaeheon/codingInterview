503b6d217269dd4382fdb315b7940e34cb454ccb
2jaeheon <jaeheon0826@jbnu.ac.kr>
2025-01-13 17:29:18 +0900

    ADD: 트리 순회

---
+++ Practice/src/hash/num25/트리_순회.java
@@ -0,0 +1,43 @@
+package hash.num25;
+
+import java.util.Arrays;
+
+public class 트리_순회 {
+
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(solution(new int[]{1, 2, 3, 4, 5, 6, 7})));
+    }
+
+    public static String[] solution(int[] nodes) {
+        String preOrderString = preOrder(nodes, 0);
+        String inOrderString = inOrder(nodes, 0);
+        String postOrderString = postOrder(nodes, 0);
+
+        return new String[]{preOrderString, inOrderString, postOrderString};
+    }
+
+
+    public static String preOrder(int[] nodes, int index) {
+        if (index >= nodes.length) {
+            return "";
+        }
+
+        return nodes[index] + preOrder(nodes, 2 * index + 1) + preOrder(nodes, 2 * index + 2);
+    }
+
+    public static String inOrder(int[] nodes, int index) {
+        if (index >= nodes.length) {
+            return "";
+        }
+
+        return inOrder(nodes, 2 * index + 1) + nodes[index] + inOrder(nodes, 2 * index + 2);
+    }
+
+    public static String postOrder(int[] nodes, int index) {
+        if (index >= nodes.length) {
+            return "";
+        }
+
+        return postOrder(nodes, 2 * index + 1) + postOrder(nodes, 2 * index + 2) + nodes[index];
+    }
+}
--- Practice/src/queue/요세푸스문제/요세푸스문제.java
+++ Practice/src/queue/요세푸스문제/요세푸스문제.java
@@ -1,56 +1,56 @@
-package queue.요세푸스문제;
-
-import java.util.ArrayDeque;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Queue;
-
-public class 요세푸스문제 {
-
-    public static void main(String[] args) {
-        int N = 5;
-        int k = 2;
-
-        System.out.println(solution(N, k));
-    }
-
-    public static int solution(int n, int k) {
-        int answer = 0;
-        Queue<Integer> queue = new ArrayDeque<>();
-
-        for (int i = 1; i <= n; i++) {
-            queue.add(i);
-        }
-
-        //k번 반복해서 앞에서 빼서 뒤로 넣고 하나를 없앰, 이 과정을 1개보다 클 때까지 반복
-        while (queue.size() > 1) {
-            for (int j = 1; j < k; j++) {
-                queue.add(queue.poll());
-            }
-            queue.poll();
-        }
-
-        //큐에 남아있는 마지막 원소 하나를 반환
-        return queue.peek();
-    }
-
-    public static int solution2(int n, int k) {
-        List<Integer> list = new ArrayList<>();
-
-        //리스트에 1부터 n까지의 값 초기화
-        for (int i = 1; i <= n; i++) {
-            list.add(i);
-        }
-
-        //원소의 값은 1부터 시작하지만 인덱스값은 0부터 시작이기 때문에 index = 0
-        int index = 0;
-        int answer = 0;
-        while (list.size() > 1) {
-            index = (index + k - 1) % list.size();
-            answer = list.get(index);
-            list.remove(index);
-        }
-
-        return list.getFirst();
-    }
-}
+//package queue.요세푸스문제;
+//
+//import java.util.ArrayDeque;
+//import java.util.ArrayList;
+//import java.util.List;
+//import java.util.Queue;
+//
+//public class 요세푸스문제 {
+//
+//    public static void main(String[] args) {
+//        int N = 5;
+//        int k = 2;
+//
+//        System.out.println(solution(N, k));
+//    }
+//
+//    public static int solution(int n, int k) {
+//        int answer = 0;
+//        Queue<Integer> queue = new ArrayDeque<>();
+//
+//        for (int i = 1; i <= n; i++) {
+//            queue.add(i);
+//        }
+//
+//        //k번 반복해서 앞에서 빼서 뒤로 넣고 하나를 없앰, 이 과정을 1개보다 클 때까지 반복
+//        while (queue.size() > 1) {
+//            for (int j = 1; j < k; j++) {
+//                queue.add(queue.poll());
+//            }
+//            queue.poll();
+//        }
+//
+//        //큐에 남아있는 마지막 원소 하나를 반환
+//        return queue.peek();
+//    }
+//
+//    public static int solution2(int n, int k) {
+//        List<Integer> list = new ArrayList<>();
+//
+//        //리스트에 1부터 n까지의 값 초기화
+//        for (int i = 1; i <= n; i++) {
+//            list.add(i);
+//        }
+//
+//        //원소의 값은 1부터 시작하지만 인덱스값은 0부터 시작이기 때문에 index = 0
+//        int index = 0;
+//        int answer = 0;
+//        while (list.size() > 1) {
+//            index = (index + k - 1) % list.size();
+//            answer = list.get(index);
+//            list.remove(index);
+//        }
+//
+//        return list.getFirst();
+//    }
+//}
