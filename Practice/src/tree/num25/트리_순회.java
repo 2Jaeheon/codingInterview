b59ed7f7e0b668d5f7fd8ce0fada1eed7e1bebb5
2jaeheon <jaeheon0826@jbnu.ac.kr>
2025-05-09 11:12:58 +0900

    review: 트리 순회 (전위, 중위, 후위 재귀로 구현)

--- Practice/src/tree/num25/트리_순회.java
+++ Practice/src/tree/num25/트리_순회.java
@@ -5,39 +5,40 @@ import java.util.Arrays;
 public class 트리_순회 {
 
     public static void main(String[] args) {
-        System.out.println(Arrays.toString(solution(new int[]{1, 2, 3, 4, 5, 6, 7})));
+        int[] arr = {1, 2, 3, 4, 5, 6, 7};
+        System.out.println(Arrays.toString(solution(arr)));
     }
 
-    public static String[] solution(int[] nodes) {
-        String preOrderString = preOrder(nodes, 0);
-        String inOrderString = inOrder(nodes, 0);
-        String postOrderString = postOrder(nodes, 0);
+    public static String[] solution(int[] arr) {
+        String[] result = new String[3];
+        result[0] = preOrder(arr, 0);
+        result[1] = inOrder(arr, 0);
+        result[2] = postOrder(arr, 0);
 
-        return new String[]{preOrderString, inOrderString, postOrderString};
+        return result;
     }
 
-
-    public static String preOrder(int[] nodes, int index) {
-        if (index >= nodes.length) {
+    public static String preOrder(int[] arr, int index) {
+        if (index >= arr.length) {
             return "";
         }
 
-        return nodes[index] + preOrder(nodes, 2 * index + 1) + preOrder(nodes, 2 * index + 2);
+        return arr[index] + preOrder(arr, index * 2 + 1) + preOrder(arr, index * 2 + 2);
     }
 
-    public static String inOrder(int[] nodes, int index) {
-        if (index >= nodes.length) {
+    public static String inOrder(int[] arr, int index) {
+        if (index >= arr.length) {
             return "";
         }
 
-        return inOrder(nodes, 2 * index + 1) + nodes[index] + inOrder(nodes, 2 * index + 2);
+        return inOrder(arr, index * 2 + 1) + arr[index] + inOrder(arr, index * 2 + 2);
     }
 
-    public static String postOrder(int[] nodes, int index) {
-        if (index >= nodes.length) {
+    public static String postOrder(int[] arr, int index) {
+        if (index >= arr.length) {
             return "";
         }
 
-        return postOrder(nodes, 2 * index + 1) + postOrder(nodes, 2 * index + 2) + nodes[index];
+        return postOrder(arr, index * 2 + 1) + inOrder(arr, index * 2 + 2) + arr[index];
     }
 }
