249e1ba4f8baac4f0552dac85455d1a55b377614
2jaeheon <jaeheon0826@jbnu.ac.kr>
2025-05-14 23:33:32 +0900

    review:길찾기 게임

---
+++ Practice/src/tree/num29/review_길찾기_게임.java
@@ -0,0 +1,110 @@
+package tree.num29;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import tree.num29.길_찾기_게임.Node;
+
+public class review_길찾기_게임 {
+
+    public static void main(String[] args) {
+        int[][] nodeinfo = new int[][]{
+            {5, 3}, {11, 5}, {13, 3}, {3, 5}, {6, 1}, {1, 3}, {8, 6}, {7, 2}, {2, 2}};
+        System.out.println(Arrays.deepToString(solution(nodeinfo)));
+    }
+
+    public static class Node {
+
+        int num;
+        int x;
+        int y;
+        Node left, right;
+
+        public Node(int num, int x, int y) {
+            this.x = x;
+            this.y = y;
+            this.num = num;
+        }
+    }
+
+    public static int[][] solution(int[][] nodeInfo) {
+        Node[] nodes = new Node[nodeInfo.length];
+
+        // 각 노드에 좌표 및 번호를 저장함.
+        for (int i = 0; i < nodeInfo.length; i++) {
+            nodes[i] = new Node(i + 1, nodeInfo[i][0], nodeInfo[i][1]);
+        }
+
+        // y를 기준으로 내림차순 정렬
+        Arrays.sort(nodes, (o1, o2) -> {
+            if (o1.y == o2.y) {
+                return Integer.compare(o1.x, o2.x);
+            }
+            return Integer.compare(o2.y, o1.y);
+        });
+
+        // 맨 처음 노드는 루트
+        Node root = nodes[0];
+
+        // binary 트리를 구성하는 과정
+        for (int i = 1; i < nodes.length; i++) {
+            // parent pointer를 만들어서 root
+            Node parent = root;
+
+            while (true) {
+                // 부모 노드의 x 좌료가 더 크면 왼쪽
+                if (nodes[i].x < parent.x) {
+                    if (parent.left == null) {
+                        parent.left = nodes[i];
+                        break;
+                    } else {
+                        parent = parent.left;
+                    }
+                } else { // 부모 노드의 x 좌표가 더 작거나 같으면 오른쪽
+                    if (parent.right == null) {
+                        parent.right = nodes[i];
+                        break;
+                    } else {
+                        parent = parent.right;
+                    }
+                }
+            }
+        }
+
+        // 순회 결과 저장용 리스트
+        List<Integer> preorderList = new ArrayList<>();
+        List<Integer> postorderList = new ArrayList<>();
+
+        preorder(root, preorderList);
+        postorder(root, postorderList);
+
+        // 결과 변환
+        int[][] result = new int[2][nodeInfo.length];
+        for (int i = 0; i < nodeInfo.length; i++) {
+            result[0][i] = preorderList.get(i);
+            result[1][i] = postorderList.get(i);
+        }
+
+        return result;
+    }
+
+    // 전위 순회
+    public static void preorder(Node node, List<Integer> result) {
+        if (node == null) {
+            return;
+        }
+        result.add(node.num);
+        preorder(node.left, result);
+        preorder(node.right, result);
+    }
+
+    // 후위 순회
+    public static void postorder(Node node, List<Integer> result) {
+        if (node == null) {
+            return;
+        }
+        postorder(node.left, result);
+        postorder(node.right, result);
+        result.add(node.num);
+    }
+}
