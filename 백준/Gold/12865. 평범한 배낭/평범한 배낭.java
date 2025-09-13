875c9330e6a738e477d20487558d6ea63f4bf3bc
JaeheonLee <121741455+2Jaeheon@users.noreply.github.com>
2025-09-14 00:43:37 +0900

    [Gold V] Title: 평범한 배낭, Time: 260 ms, Memory: 57224 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
@@ -4,62 +4,29 @@ public class Main{
     
     static int N;
     static int K;
-    static int[] weights;
-    static int[] values;
     static int[][] dp;
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
 
-        // 여행에 필요하다고 생각하는 N개의 물건
-        // 각 물건은 Weight와 Value를 가짐
-        // 해당 물건을 가지면 V만큼 즐길 수 있음
-        // K만큼의 무게만을 넣을 수 있음
-
-        // 가방문제!
         N = sc.nextInt();
         K = sc.nextInt();
-        weights = new int[N];
-        values = new int[N];
-        dp = new int[N][K + 1];
-        for(int i = 0; i < N; i++) {
-            Arrays.fill(dp[i], -1);
-        }
-        // 어떻게 풀어야할까?
-        // 전형적인 DP류의 문제로 보임
-        // 메모이제이션 + DFS로 풀 수 있을 것 같음
-        
-        // N개의 물건들을 순회
-        for(int i = 0; i < N; i++) {
-            int w = sc.nextInt();
-            int v = sc.nextInt();
+        dp = new int[N + 1][K + 1];
 
-            weights[i] = w;
-            values[i] = v;
-        }
+        for(int i = 1; i <= N; i++) { // i: 물건 인덱스
+            int weight = sc.nextInt();
+            int value = sc.nextInt();
 
-        System.out.println(dfs(0, K));
-    }
-
-    public static int dfs(int idx, int leftWeight) {
-        if (idx == N) {
-            return 0;
-        }
-
-        // memoization
-        if (dp[idx][leftWeight] != -1) {
-            return dp[idx][leftWeight];
-        }
-
-        // 값을 취하지 않았을 때의 값
-        int value_when_not_taken = dfs(idx + 1, leftWeight);
-
-        // 값을 취했을 때의 값
-        int value_when_taken = 0;
-        if (weights[idx] <= leftWeight) {
-            value_when_taken = values[idx] + dfs(idx + 1, leftWeight - weights[idx]);
+            for(int j = 1; j <= K; j++) { // j: 배낭의 무게 한도
+                if(weight > j) {
+                    dp[i][j] = dp[i - 1][j];
+                } else {
+                    int value_not_taken = dp[i - 1][j];
+                    int value_taken = value + dp[i - 1][j - weight];
+                    dp[i][j] = Math.max(value_not_taken, value_taken);
+                }
+            }
         }
         
-        // 담았을 때와 담지않았을 때의 가장 큰 가치를 dp에 저장하고 반환
-        return dp[idx][leftWeight] = Math.max(value_when_not_taken, value_when_taken);
+        System.out.println(dp[N][K]);
     }
 }
