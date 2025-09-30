2b3d1c3951e168cb39b1db5b9c8357571833f045
JaeheonLee <121741455+2Jaeheon@users.noreply.github.com>
2025-09-30 15:11:14 +0900

    [Gold IV] Title: 숨바꼭질 2, Time: 228 ms, Memory: 28312 KB -BaekjoonHub

--- 백준/Gold/12851. 숨바꼭질 2/숨바꼭질 2.java
+++ 백준/Gold/12851. 숨바꼭질 2/숨바꼭질 2.java
@@ -16,41 +16,38 @@ public class Main{
         int[] visited = new int[100001]; // 방문까지의 최소시간을 기록할 용도
         Arrays.fill(visited, -1);
 
-        queue.add(n);
+        queue.offer(n);
         visited[n] = 0;
 
         int minTime = -1; // 가장 빠른 시간
         int count = 0;    // 방법의 수
         
         while(!queue.isEmpty()) {
-            int currentPosition = queue.poll();
-            int currentTime = visited[currentPosition];
+            int curPosition = queue.poll();
+            int curTime =  visited[curPosition];
 
-            // 이미 찾은 가장 빠른 시간보다 현재 시간이 더 오래 걸리면 더 이상 탐색할 필요가 없음
-            if(minTime != -1 && currentTime > minTime) {
+            if(minTime != -1 && curTime > minTime) {
                 break;
             }
 
             // 동생을 찾았을 때
-            if (currentPosition == k) {
-                // 처음 찾은 경우
-                if (minTime == -1) {
-                    minTime = currentTime;
+            if(curPosition == k) {
+                if(minTime == -1) {
+                    minTime = curTime;
                 }
-                // 가장 빠른 시간과 동일한 시간에 도착한 경우, 방법의 수 증가
-                if (minTime == currentTime) {
+
+                if(minTime == curTime) {
                     count++;
                 }
             }
-            
-            int[] nextPositions = {currentPosition - 1, currentPosition + 1, currentPosition * 2};
 
-            for (int nextPos : nextPositions) {
-                // 위치가 유효한 범위 내에 있는지 확인
-                if (nextPos >= 0 && nextPos <= 100_000) {
-                    if (visited[nextPos] == -1 || visited[nextPos] == currentTime + 1) {
-                        visited[nextPos] = currentTime + 1;
-                        queue.add(nextPos);
+            
+            int[] nextPositions = new int[]{curPosition - 1, curPosition + 1, curPosition * 2};
+            for(int nextPosition : nextPositions) {
+                if (nextPosition >= 0 && nextPosition <= 100_000) {
+                    if(visited[nextPosition] == -1 || visited[nextPosition] == curTime + 1) {
+                        visited[nextPosition] = curTime + 1;
+                        queue.offer(nextPosition);
                     }
                 }
             }
