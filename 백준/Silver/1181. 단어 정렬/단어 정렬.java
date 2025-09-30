7ac6dc0d8f30df309db459f1a0f8f79a25f7bf29
JaeheonLee <121741455+2Jaeheon@users.noreply.github.com>
2025-09-30 15:24:40 +0900

    [Silver V] Title: 단어 정렬, Time: 808 ms, Memory: 58252 KB -BaekjoonHub

---
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class Main{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        sc.nextLine();
+        Set<String> set = new HashSet<>();
+        
+        for(int i = 0; i < n; i++) {
+            set.add(sc.nextLine());
+        }
+        
+        String[] strArr = set.toArray(new String[0]);
+        Arrays.sort(strArr);
+        Arrays.sort(strArr, Comparator.comparingInt(a -> a.length()));
+
+        for(String str : strArr) {
+            System.out.println(str);
+        }
+    }
+}
