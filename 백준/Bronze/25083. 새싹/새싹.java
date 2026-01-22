78fb4d9e963c7f729da9b3c13ce9c2c5dc88e4a6
JaeheonLee <121741455+2Jaeheon@users.noreply.github.com>
2026-01-22 10:43:11 +0900

    [Bronze V] Title: 새싹, Time: 160 ms, Memory: 17360 KB -BaekjoonHub

--- 백준/Bronze/25083. 새싹/새싹.java
+++ 백준/Bronze/25083. 새싹/새싹.java
@@ -1,7 +1,11 @@
-public class Main {
-    public static void main(String[] args){
+import java.util.*;
+
+public class Main{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+
         System.out.println("         ,r\'\"7");
-        System.out.println("r`-_   ,'  ,/");
+        System.out.println("r`-_   ,\'  ,/");
         System.out.println(" \\. \". L_r\'");
         System.out.println("   `~\\/");
         System.out.println("      |");
