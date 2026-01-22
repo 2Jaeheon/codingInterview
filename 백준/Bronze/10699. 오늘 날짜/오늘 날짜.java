d4b622cc3ab0f120f1f2db659e26fba1fd8455c4
JaeheonLee <121741455+2Jaeheon@users.noreply.github.com>
2026-01-22 10:28:42 +0900

    [Bronze V] Title: 오늘 날짜, Time: 240 ms, Memory: 19960 KB -BaekjoonHub

--- 백준/Bronze/10699. 오늘 날짜/오늘 날짜.java
+++ 백준/Bronze/10699. 오늘 날짜/오늘 날짜.java
@@ -1,7 +1,13 @@
+import java.util.*;
 import java.time.*;
+import java.time.format.DateTimeFormatter;
+
 public class Main{
-    public static void main(String[] args){
-        LocalDate now = LocalDate.now();
-        System.out.println(now);
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        LocalDate ldt = LocalDate.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("YYYY-MM-dd");
+
+        System.out.println(ldt.format(formatter));
     }
 }
